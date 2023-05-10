a5793238719d5d9ad0cd523e2e8d93df41788c08
denis-myroshnykov <68814867+denis-myroshnykov@users.noreply.github.com>
2023-05-10 14:09:48 +0300

    Add files via upload

---
+++ API7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Random random = new Random();
+
+        int id = random.Next(1, 101);
+
+        string url = $"https://anapioficeandfire.com/api/characters/{id}";
+
+        using (var client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+
+                User user = JsonSerializer.Deserialize<User>(json);
+
+                Console.WriteLine($"Iм'я: {user.name}");
+                Console.WriteLine($"Стать: {user.gender}");
+                Console.WriteLine($"Культура: {user.culture}");
+            }
+            else
+            {
+                Console.WriteLine($"Помилка запиту: {response.StatusCode}");
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
+
+class User
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string gender { get; set; }
+    public string culture { get; set; }
+}
